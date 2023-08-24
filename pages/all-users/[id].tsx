2fc62a24e0773ef09b05a8d68a2af4a10aa13da6
amandeep-codebin <amandeep@code-b.in>
2023-08-24 17:35:41 +0530

    used getStaticPaths for incremental static regeneration

--- pages/all-users/[id].tsx
+++ pages/all-users/[id].tsx
@@ -2,13 +2,36 @@ import { UserDataProps } from '@/types'
 import Link from 'next/link'
 import React from 'react'
 
+
+export  async function getStaticPaths (){
+
+  const response = await fetch('https://jsonplaceholder.typicode.com/users')
+  const user = await response.json()
+
+  const paths = user.map((item:UserDataProps)=>({
+    params:{id: item.id.toString()}
+  }))
+
+  return { paths, fallback: 'blocking' }
+}
+
+export const getStaticProps = async({params}:any)=>{
+
+  const {id} = params
+  const response = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+  const res = await response.json()
+
+  return{props: {
+    user:res
+  }}
+}
 const SingleUser = ({user}:{user: UserDataProps}) => {
 
   return (
     <>
     {user ? 
     <div>
-      <h1> Name: {user.name}</h1>
+      <h1>Name: {user.name}</h1>
       <h2>Email: {user.email}</h2>
       <h3>Phone: {user.phone}</h3>
       <p>Address: {user?.address?.street}</p>
@@ -23,16 +46,6 @@ const SingleUser = ({user}:{user: UserDataProps}) => {
      </div> }
     </>
   )
-}
-export const getStaticProps = async({params}:any)=>{
-
-  const {id} =params
-  const response = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
-  const res = await response.json()
-
-  return{props: {
-    user:res
-  }}
 }
 
 export default SingleUser
