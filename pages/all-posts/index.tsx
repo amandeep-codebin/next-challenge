b35af9fdfc365d567c11b675e61a6f80649f66d9
amandeep-codebin <amandeep@code-b.in>
2023-08-24 12:13:17 +0530

    implemented post by user page functionaluty

--- pages/all-posts/index.tsx
+++ pages/all-posts/index.tsx
@@ -1,24 +1,38 @@
-import { UserDataProps, UserPostDataType } from "@/types"
+import { UserPostDataType } from "@/types"
 import Link from "next/link"
 import { useRouter } from "next/router"
 import React, { useEffect, useState } from "react"
 
 const Posts =  ({posts}:{posts: UserPostDataType[]}) => {
 
-  const [userPosts,setUserPosts] = useState(posts)
+  const [userPosts,setUserPosts] = useState<UserPostDataType[]>(posts)
 
   const router = useRouter()
-  console.log('router',router.query)
 
   const {userId} = router.query
 
+  console.log('userId',userId)
+
+  useEffect(()=>{
+    if(userId){
+      const fetchUserPost = async() =>{
+        const response = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`)
+
+        if(response.status === 200){
+          const data = await response.json()
+          setUserPosts(data)
+        }
+      }
+      fetchUserPost()
+    }
+  },[])
 
   return (
     <div>
       <h1>Post Page</h1>
 
     <ul>
-      {posts && posts.map((item: UserPostDataType)=> (
+      {userPosts && userPosts.map((item: UserPostDataType)=> (
         <li key={item.id}><Link href={`/all-posts/${item.id}`}>{item.title}</Link></li>
       ))}
     </ul>
